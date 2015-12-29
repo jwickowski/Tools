eb5fc62c404fb020ebac77ee3be651dee152c757
Jerzy Wickowski <tazos333@gmail.com>
2015-12-29 22:09:24 +0100

    clear usings

--- Tazos.Tools.Extensions.EntityFramework/IncludeExtenstions.cs
+++ Tazos.Tools.Extensions.EntityFramework/IncludeExtenstions.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.Entity;
-using System.Data.Entity.Core.Objects;
 using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Linq.Expressions;
