cee2c1811c6c6494619959fdd5470c6d8891a3f7
Jerzy Wickowski <tazos333@gmail.com>
2017-10-29 14:55:31 +0100

    make Add-DataTakenToFilesName more generic

--- Tazos.Tools.PowerShell/Add-DataTakenToFilesName.ps1
+++ Tazos.Tools.PowerShell/Add-DataTakenToFilesName.ps1
@@ -1,4 +1,36 @@
- function Add-DataTakenToFilesName{
+function Get-DataTaken{
+param ($fileMetadata)
+$rawDate = $fileMetadata.'Data wykonania'
+$rawDate = $rawDate -replace '[^a-zA-Z0-9 :.]', '' #remove non date format characters
+$datePattern = [System.Threading.Thread]::CurrentThread.CurrentUICulture.DateTimeFormat.ShortDatePattern;
+$timePattern = [System.Threading.Thread]::CurrentThread.CurrentUICulture.DateTimeFormat.ShortTimePattern;
+$pattern = "$datePattern $timePattern"
+$culture = [System.Globalization.CultureInfo]::InvariantCulture 
+
+$parsedDate = [DateTime]::ParseExact($rawDate, $pattern, $culture)
+
+$formatedDate = $parsedDate.ToString('yyyy.mm.dd HH-mm');
+$formatedDate 
+}
+
+function Get-Extension{
+param ($fileMetadata)
+$rawExtension= $fileMetadata.'Rozszerzenie Pliku'
+$rawExtension
+}
+function Get-FileName{
+param ($fileMetadata)
+$name = $fileMetadata.'Nazwa'
+$extenstion = Get-Extension -fileMetadata $fileMetadata
+if($name.EndsWith($extenstion) )
+		{
+			$name
+		}
+$name = $name + $extenstion
+$name
+}
+ 
+function Add-DataTakenToFilesName{
  param(
 	[Parameter(Mandatory)]
 	[string] 
@@ -7,13 +39,13 @@
 	$filesMetadata = Get-FileMetaData -folder $Path
 	foreach($fileMetadata in $filesMetadata)
 	{
-		$dateTaken = $fileMetadata.'Date taken'
+		$dateTaken = Get-DataTaken -fileMetadata $fileMetadata
 		if (-not $dateTaken)
 		{
 			continue;
 		}
-        $dateTaken = $dateTaken.Replace('-', '.').Replace(':', '-')
-		$fileName = $fileMetadata.'Name'
+       
+		$fileName = Get-FileName -fileMetadata $fileMetadata
 
 		if($fileName.StartsWith($dateTaken) )
 		{
@@ -28,3 +60,4 @@
 
 	}
 }
+
